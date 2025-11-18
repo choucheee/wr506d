7a11726b866849baf70449308d89d7d1acdbf83b
Paulooo10 <paulo10ytb@gmail.com>
2025-11-18 08:53:42 +0100

    feat: Ajout du service SlugifierService pour slugifier les titres de produits
    - Création du service SlugifierService utilisant Symfony String
    - Intégration du service dans ProductController
    - Affichage du titre et du slug dans le template view.html.twig
    - Exemple: 'T-Shirt d'Été !' -> 't-shirt-d-ete'

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\SlugifierService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,6 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
+    public function __construct(
+        private readonly SlugifierService $slugifierService
+    ) {
+    }
+
     #[Route('/products', name: 'app_products_list')]
     public function listProducts(): Response
     {
@@ -21,8 +27,16 @@ final class ProductController extends AbstractController
         // Récupération du paramètre id depuis la requête (paramètre de route)
         $id = $request->attributes->get('id');
         
+        // Exemple de titre produit
+        $productTitle = "T-Shirt d'Été !";
+        
+        // Slugification du titre
+        $slug = $this->slugifierService->slugify($productTitle);
+        
         return $this->render('product/view.html.twig', [
             'id' => $id,
+            'title' => $productTitle,
+            'slug' => $slug,
         ]);
     }
 }
